1d6b74f68f0c296310e0db5a99b1e17a1e39d42f
PhilipDeFraties <Philipjfry338@gmail.com>
2020-12-12 20:05:13 -0700

    Add relationships to item serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,7 @@
 class ItemSerializer < BaseSerializer
   attributes :name, :description, :unit_price, :merchant_id
   belongs_to :merchant
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
 end
