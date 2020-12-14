bbea5fa0d8fd527ae1c2fdced48b53ac8b32f7a0
PhilipDeFraties <Philipjfry338@gmail.com>
2020-12-13 18:20:11 -0700

    Adds items route under merchants resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :items
-      resources :merchants
+      resources :merchants do
+        get :items, to: 'merchants/items#index'
+      end
     end
   end
 end
