98e996b09441ff19941f4d95fd66a4c49963890c
PhilipDeFraties <Philipjfry338@gmail.com>
2020-12-13 20:06:21 -0700

    Adds route for items/find_all endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      get 'items/find_all', to: 'items/search#index'
+
       resources :items do
         get :merchants, to: 'items/merchants#show'
       end
+      
       resources :merchants do
         get :items, to: 'merchants/items#index'
       end
