84c08fef21b879e474a97575c5aed903ae96cced
PhilipDeFraties <Philipjfry338@gmail.com>
2020-12-13 18:36:10 -0700

    Adds merchants show route under items resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :items
+      resources :items do
+        get :merchants, to: 'items/merchants#show'
+      end
       resources :merchants do
         get :items, to: 'merchants/items#index'
       end
