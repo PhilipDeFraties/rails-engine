a7706c9a214899f7c1a6e48ab0d086534cb01e47
PhilipDeFraties <Philipjfry338@gmail.com>
2020-12-15 16:38:58 -0700

    Add timestamps to item serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,9 @@
 class ItemSerializer < BaseSerializer
-  attributes :name, :description, :unit_price, :merchant_id
+  attributes :name,
+             :description,
+             :unit_price,
+             :merchant_id,
+             :created_at,
+             :updated_at
   belongs_to :merchant
 end
