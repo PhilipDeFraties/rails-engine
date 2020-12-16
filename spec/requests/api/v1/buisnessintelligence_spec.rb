0b96c8b0dc3b556ad0a7fe4f6995275aa6e3f48f
PhilipDeFraties <Philipjfry338@gmail.com>
2020-12-16 16:53:15 -0700

    Adds spec for biz intel with test for merchants by most revenue

---
+++ spec/requests/api/v1/buisnessintelligence_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+describe 'Buisness Intelligence Endpoints' do
+  describe 'Merchants with Most Revenue' do
+    before :each do
+      @mer_1 = create :merchant
+      @mer_2 = create :merchant
+      @mer_3 = create :merchant
+      @mer_4 = create :merchant
+      @mer_5 = create :merchant
+      @mer_6 = create :merchant
+      @mer_7 = create :merchant
+
+      @item_1 = create(:item, unit_price: 1.00, merchant: @mer_1)
+      @item_2 = create(:item, unit_price: 1.00, merchant: @mer_2)
+      @item_3 = create(:item, unit_price: 1.00, merchant: @mer_3)
+      @item_4 = create(:item, unit_price: 1.00, merchant: @mer_4)
+      @item_5 = create(:item, unit_price: 1.00, merchant: @mer_5)
+      @item_6 = create(:item, unit_price: 1.00, merchant: @mer_6)
+      @item_7 = create(:item, unit_price: 1.00, merchant: @mer_7)
+
+      @invoice_1 = create(:invoice, merchant: @mer_1)
+      @invoice_2 = create(:invoice, merchant: @mer_2)
+      @invoice_3 = create(:invoice, merchant: @mer_3)
+      @invoice_4 = create(:invoice, merchant: @mer_4)
+      @invoice_5 = create(:invoice, merchant: @mer_5)
+      @invoice_6 = create(:invoice, merchant: @mer_6)
+      @invoice_7 = create(:invoice, merchant: @mer_7, status: 'packaged')
+
+      @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1, quantity: 5, unit_price: 1.00)
+      @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice_2, quantity: 4, unit_price: 1.00)
+      @invoice_item_3 = create(:invoice_item, item: @item_3, invoice: @invoice_3, quantity: 6, unit_price: 1.00)
+      @invoice_item_4 = create(:invoice_item, item: @item_4, invoice: @invoice_4, quantity: 1, unit_price: 1.00)
+      @invoice_item_5 = create(:invoice_item, item: @item_5, invoice: @invoice_5, quantity: 2, unit_price: 1.00)
+      @invoice_item_6 = create(:invoice_item, item: @item_6, invoice: @invoice_6, quantity: 3, unit_price: 1.00)
+      @invoice_item_7 = create(:invoice_item, item: @item_6, invoice: @invoice_7, quantity: 7, unit_price: 1.00)
+
+      @transaction_1 = create(:transaction, invoice: @invoice_1)
+      @transaction_2 = create(:transaction, invoice: @invoice_2)
+      @transaction_3 = create(:transaction, invoice: @invoice_3)
+      @transaction_4 = create(:transaction, invoice: @invoice_4)
+      @transaction_5 = create(:transaction, invoice: @invoice_5)
+      @transaction_6 = create(:transaction, invoice: @invoice_6)
+      @transaction_7 = create(:transaction, invoice: @invoice_7, result: 'pending')
+
+
+    end
+
+    it "can return a variable number of merchants ranked by total revenue " do
+      get '/api/v1/merchants/most_revenue?quantity=5'
+
+       expect(response).to be_successful
+
+       merchants = JSON.parse(response.body, symbolize_names: true)
+
+       expect(response).to be_successful
+
+       expect(merchants).to have_key(:data)
+       expect(merchants[:data]).to be_an(Array)
+       expect(Merchant.all.count).to eq(7)
+       expect(merchants[:data].count).to eq(5)
+
+       expected_ids_by_revenue = [@mer_3.id, @mer_1.id, @mer_2.id, @mer_6.id, @mer_5.id]
+       resulting_ids_by_revenue = []
+       merchants[:data].each do |merchant|
+         merchant_response_checker(merchant)
+         resulting_ids_by_revenue << merchant[:id].to_i
+       end
+
+       expect(resulting_ids_by_revenue).to eq(expected_ids_by_revenue)
+    end
+  end
+end
