d61157eac089c464002dd96f970fc5b58db749f1
PhilipDeFraties <Philipjfry338@gmail.com>
2020-12-16 16:54:44 -0700

    Adds merchant method for finding merchants ranked by revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,15 @@ class Merchant < ApplicationRecord
   has_many :invoices, :dependent => :delete_all
   has_many :invoice_items, through: :invoices
   has_many :transactions, through: :invoices
+
+
+  def self.rank_by_revenue(quantity)
+    select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+      .joins(invoices: [:invoice_items, :transactions])
+      .where("transactions.result = 'success' AND invoices.status = 'shipped'")
+      .group(:id)
+      .order("revenue DESC")
+      .limit(quantity)
+  end
+
 end
