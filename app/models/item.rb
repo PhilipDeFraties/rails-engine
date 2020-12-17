8c38a69b862509e3d265cc15f582c1ce4d531348
PhilipDeFraties <Philipjfry338@gmail.com>
2020-12-17 15:16:31 -0700

    Adds validations to item model, removes import method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,9 @@
 class Item < ApplicationRecord
+  validates_presence_of :name, :description, :unit_price
+  validates_numericality_of :unit_price, greater_than: 0
+
   belongs_to :merchant
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
-
-
-  def self.import_items
-    items = []
-    CSV.foreach('db/data/items.csv', headers: true) do |row|
-      row["unit_price"] = convert_price(row["unit_price"])
-      items << row.to_h
-    end
-    Item.import(items)
-  end
-
-  def self.convert_price(price)
-    (price.to_i * 0.01).round(2)
-  end
 end
