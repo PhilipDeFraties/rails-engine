c9ce4402dd06ec6b8ed8a43a3ec459a9ba9b4875
PhilipDeFraties <Philipjfry338@gmail.com>
2020-12-16 18:07:57 -0700

    Adds revenue action to merchants::bizintel controller

--- app/controllers/api/v1/merchants/bizintel_controller.rb
+++ app/controllers/api/v1/merchants/bizintel_controller.rb
@@ -10,4 +10,9 @@ class Api::V1::Merchants::BizintelController < ApplicationController
     render json: MerchantSerializer.new(merchants)
   end
 
+  def revenue
+    revenue = Merchant.revenue(params[:id])
+    render json: RevenueSerializer.revenue(revenue)
+  end
+
 end
