929696ca2352a6aac4c538e5105eb52d1aa2ed5e
PhilipDeFraties <Philipjfry338@gmail.com>
2020-12-16 19:15:55 -0700

    Adds revenue_across_dates action to bizintel_controller

--- app/controllers/api/v1/merchants/bizintel_controller.rb
+++ app/controllers/api/v1/merchants/bizintel_controller.rb
@@ -1,17 +1,22 @@
 class Api::V1::Merchants::BizintelController < ApplicationController
 
   def most_revenue
-    merchants = Merchant.rank_by_revenue(params[:quantity])
+    merchants = Merchant.most_revenue(params[:quantity])
     render json: MerchantSerializer.new(merchants)
   end
 
   def most_items
-    merchants = Merchant.rank_by_items_sold(params[:quantity])
+    merchants = Merchant.most_items_sold(params[:quantity])
     render json: MerchantSerializer.new(merchants)
   end
 
   def revenue
     revenue = Merchant.revenue(params[:id])
+    render json: RevenueSerializer.revenue(revenue)
+  end
+
+  def revenue_across_dates
+    revenue = InvoiceItem.revenue_accross_dates(params[:start], params[:end])
     render json: RevenueSerializer.revenue(revenue)
   end
 
