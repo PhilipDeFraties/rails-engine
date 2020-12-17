d3705796e8c32f111b91dc2883bd5ab68554ae9e
PhilipDeFraties <Philipjfry338@gmail.com>
2020-12-17 14:01:50 -0700

    Adds name validation to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,6 +4,8 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :invoices
   has_many :transactions, through: :invoices
 
+  validates_presence_of :name
+
   def self.most_revenue(quantity)
     select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
       .joins(invoices: [:invoice_items, :transactions])
