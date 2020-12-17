bc297423abee08ef80533032e8c003241f77da03
PhilipDeFraties <Philipjfry338@gmail.com>
2020-12-16 17:26:36 -0700

    Adds action for most_items to merchants::bizintel controller

--- app/controllers/api/v1/merchants/bizintel_controller.rb
+++ app/controllers/api/v1/merchants/bizintel_controller.rb
@@ -5,4 +5,9 @@ class Api::V1::Merchants::BizintelController < ApplicationController
     render json: MerchantSerializer.new(merchants)
   end
 
+  def most_items
+    merchants = Merchant.rank_by_items_sold(params[:quantity])
+    render json: MerchantSerializer.new(merchants)
+  end
+
 end
