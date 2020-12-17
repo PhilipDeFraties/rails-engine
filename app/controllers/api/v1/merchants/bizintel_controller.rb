f6c79adf8b0813d353713aa1f0d01350e8196ccf
PhilipDeFraties <Philipjfry338@gmail.com>
2020-12-16 22:44:10 -0700

    Adds params_check method to actions in biz_intel controller acting as sad path

--- app/controllers/api/v1/merchants/bizintel_controller.rb
+++ app/controllers/api/v1/merchants/bizintel_controller.rb
@@ -1,13 +1,21 @@
 class Api::V1::Merchants::BizintelController < ApplicationController
 
   def most_revenue
-    merchants = Merchant.most_revenue(params[:quantity])
-    render json: MerchantSerializer.new(merchants)
+    unless params[:quantity] && !params[:quantity].empty?
+      check_params(['quantity'])
+    else
+      merchants = Merchant.most_revenue(params[:quantity])
+      render json: MerchantSerializer.new(merchants)
+    end
   end
 
   def most_items
-    merchants = Merchant.most_items_sold(params[:quantity])
-    render json: MerchantSerializer.new(merchants)
+    unless params[:quantity] && !params[:quantity].empty?
+      check_params(['quantity'])
+    else
+      merchants = Merchant.most_items_sold(params[:quantity])
+      render json: MerchantSerializer.new(merchants)
+    end
   end
 
   def revenue
@@ -16,8 +24,25 @@ class Api::V1::Merchants::BizintelController < ApplicationController
   end
 
   def revenue_across_dates
-    revenue = InvoiceItem.revenue_accross_dates(params[:start], params[:end])
-    render json: RevenueSerializer.revenue(revenue)
+    errors = []
+    errors << 'start' unless params[:start] && !params[:start].empty?
+    errors << 'end' unless params[:end] && !params[:end].empty?
+    unless errors.empty?
+      check_params(errors)
+    else
+      render json: RevenueSerializer.revenue(InvoiceItem.revenue_accross_dates(params[:start], params[:end]))
+    end
   end
 
+private
+
+  def check_params(params)
+    render json: JSON.generate(
+      {
+        error: 'missing parameter',
+        errors: params.map { |param| "#{param} parameter required in search request" },
+        status: :bad_request
+      }
+    )
+  end
 end
