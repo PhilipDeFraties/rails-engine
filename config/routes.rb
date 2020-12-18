d69542e6173e0e4d0fdbdf08888e2f3de2360953
PhilipDeFraties <Philipjfry338@gmail.com>
2020-12-17 22:38:38 -0700

    Refactor routes to use merchants and items namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get 'items/find_all', to: 'items/search#index'
-      get 'items/find', to: 'items/search#show'
-
+      namespace :items do
+        get 'find_all', to: 'search#index'
+        get 'find', to: 'search#show'
+      end
+      
       resources :items do
         get :merchants, to: 'items/merchants#show'
       end
+
       namespace :merchants do
         get 'find_all', to: 'search#index'
         get 'find', to: 'search#show'
